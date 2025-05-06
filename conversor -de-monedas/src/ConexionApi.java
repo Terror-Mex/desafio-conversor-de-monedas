2e7dd17885f21b4ebadef004b1a8530334e17225
Adrián Luna <adrianluna012@gmail.com>
2025-05-05 21:39:10 -0600

    Agregando clase conexionApi y principal

---
+++ conversor -de-monedas/src/ConexionApi.java
@@ -0,0 +1,31 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConexionApi {
+    public static Double obtenerDatos() {
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://v6.exchangerate-api.com/v6/b11a6780cf3c74bd9aa10c82/pair/USD/MXN"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            Gson gson = new Gson();
+            JsonObject root = gson.fromJson(json, JsonObject.class);
+            return root.get("conversion_rate").getAsDouble();
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error en la solicitud" + e.getMessage());
+            return null;
+        }
+    }
+
+}
+
---
+++ conversor -de-monedas/src/Principal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        ConexionApi conexionApi = new ConexionApi();
+        System.out.println(conexionApi.obtenerDatos());
+
+
+
+
+
+    }
+}
